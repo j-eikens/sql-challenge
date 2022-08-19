5709f0d3a425257a5b11a434f8f291ad2992d401
Joey Eikens <Joey.Eikens@gmail.com>
2022-08-19 12:05:21 -0500

    finish main assignment

--- schema.sql
+++ schema.sql
@@ -89,13 +89,39 @@ join employees as e
 on (m.emp_no = e.emp_no);
 
 
-select * from dept_manager;
+-- List the department of each employee with the following information: employee number, last name, first name, and department name.
+select de.emp_no, e.last_name, e.first_name, d.dept_name
+from dept_emp as de
+join departments as d
+on (de.dept_no = d.dept_no)
+join employees as e
+on (de.emp_no = e.emp_no);
 
+--List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
+select first_name, last_name, sex
+from employees
+where first_name = 'Hercules' and last_name like 'B%';
 
+--List all employees in the Sales department, including their employee number, last name, first name, and department name.
+select d.dept_name, de.emp_no, e.first_name, e.last_name
+from departments as d
+join dept_emp as de
+on (d.dept_no = de.dept_no)
+join employees as e
+on (de.emp_no = e.emp_no)
+where d.dept_name = 'Sales';
 
---drop table departments cascade;
---drop table dept_emp cascade;
---drop table dept_manager cascade;
---drop table employees cascade;
---drop table salaries cascade;
---drop table titles cascade;
+--List all employees in the Sales and Development departments:
+--including their employee number, last name, first name, and department name.
+select d.dept_name, de.emp_no, e.first_name, e.last_name
+from departments as d
+join dept_emp as de
+on (d.dept_no = de.dept_no)
+join employees as e
+on (de.emp_no = e.emp_no)
+where d.dept_name in ('Sales', 'Development');
+
+--List the frequency count of employee last names (i.e., how many employees share each last name) in descending order.
+select last_name, count(distinct last_name) as last_name_count
+from employees
+group by last_name;
